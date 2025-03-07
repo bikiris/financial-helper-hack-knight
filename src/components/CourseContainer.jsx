a7cd6f83fdf9c75922b76da1816af41998b49cad
bikiris <a664347635@gmail.com>
2025-03-07 15:44:59 -0500

    Update styling

--- src/components/CourseContainer.jsx
+++ src/components/CourseContainer.jsx
@@ -3,16 +3,16 @@ import { Button } from '@headlessui/react';
 
 const CourseContainer = ({imageURL, title, description, internalURL}) => {
   return (
-    <div className='w-full flex flex-row justify-between items-center p-4 border border-gray-200 rounded-lg shadow-md my-4'>
+    <div className='w-full flex flex-row justify-between items-center p-4 border border-dotted border-gray-200 rounded-lg shadow-md my-4 hover:border-solid hover:border-blue-300'>
       <div className='flex flex-row items-center gap-4'>
         <img src={imageURL} alt={title+' icon'} className='max-w-[800px]'/>
-        <div className='flex flex-col'>
-          <h1>{title}</h1>
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-3xl bold'>{title}</h1>
           <p>{description}</p>
         </div>
         
       </div>
-      <Link to={internalURL} className='p-8 border'>Start Course</Link>
+      <Link to={internalURL} className='py-2 px-4 rounded-md border'>Start Course</Link>
     </div>
   )
 
