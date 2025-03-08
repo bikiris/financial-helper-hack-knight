8521a0a9532cc78fbc6b232e5086f8d33b0882d7
bikiris <a664347635@gmail.com>
2025-03-07 21:31:47 -0500

    Update styles

--- src/components/NewsContainer.jsx
+++ src/components/NewsContainer.jsx
@@ -1,15 +1,21 @@
-const NewsContainer = ({title, newsLink, imageURL}) => {
-  const openLink = () => {
-    window.open(newsLink, '_blank');
+const NewsContainer = ({ title, newsLink, imageURL }) => {
+  const openLink = (e) => {
+    window.open(newsLink, "_blank", "noopener,noreferrer");
   };
-  
+
   return (
-    <div className='flex flex-col gap-4 p-4 border border-gray-200 rounded-lg shadow-md'
-      onClick={openLink}>
-      <img src={imageURL} />
-      <h1>{title}</h1>
+    <div className='flex flex-col gap-4 p-4 border border-gray-200 rounded-lg shadow-md'>
+      <img src={imageURL} className='cursor-pointer' onClick={openLink} />
+      <div className="flex gap-4">
+        <p>News Source</p>
+        <p>Timestamps</p>
+      </div>
+
+      <a href={newsLink} className='text-2xl self-start'>
+        {title}
+      </a>
     </div>
   );
-}
+};
 
 export default NewsContainer;
