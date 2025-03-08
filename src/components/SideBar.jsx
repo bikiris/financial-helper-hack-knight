11738031066293ffdf544ca9b62e90ed368d78e3
bikiris <a664347635@gmail.com>
2025-03-07 22:40:59 -0500

    Fix hydration

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -42,19 +42,15 @@ export default function SideBar() {
                     
                     <li key={i}>
                         
-                        <a
-                          href='/'
-                          className='flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700'
+                        <Link
+                          to={link.url}
+                          className='flex items-center p-2 gap-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700'
+                          sidebar-toggle-item
                         >
-                            
                             {link.icon}
-                            <Link to={link.url}>
-                                <span className='ml-3' sidebar-toggle-item>
-                                    {link.title}
-                                </span>
-                            </Link>
-                            
-                        </a>
+                            {link.title}
+
+                        </Link>
                         
                     </li>
                 )}
