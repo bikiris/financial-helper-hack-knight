1f958b6e5c54e761404568c4aec2cc49c2bf5887
bikiris <a664347635@gmail.com>
2025-03-08 12:28:23 -0500

    Edit data

--- src/components/StockContainer.jsx
+++ src/components/StockContainer.jsx
@@ -4,9 +4,8 @@ const StockContainer = () => {
   return (
     <div className='grid grid-cols-3 gap-4'>
       <StockItem symbol='AAPL' name='Apple Inc.' stockPrice={145.9} currentValue={145.86} quantity={1} previousClose={140} purchasePrice={138}/>
-      <StockItem symbol='GOOGL' name='Alphabet Inc.' stockPrice={2765.8} currentValue={2765.8} quantity={1} previousClose={2750} purchasePrice={2700}/>
-      <StockItem symbol='MSFT' name='Microsoft Corporation' stockPrice={300.2} currentValue={300.2} quantity={1} previousClose={295} purchasePrice={290}/>
-      <StockItem symbol='AMZN' name='Amazon.com Inc.' stockPrice={3456.8} currentValue={3456.8} quantity={1} previousClose={3500} purchasePrice={3800}/>
+      <StockItem symbol='COF' name='Capital One Financial Corp.' stockPrice={150.9} currentValue={150.86} quantity={1} previousClose={140} purchasePrice={138}/>
+      <StockItem symbol='GOOGL' name='Alphabet Inc.' stockPrice={155.9} currentValue={155.86} quantity={1} previousClose={160} purchasePrice={170}/>
     </div>
   );
 }
