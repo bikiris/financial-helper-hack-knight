e0971b372f409e631cad63a259001c8e80271f87
fedy <fedy.cherif26@cuny.edu>
2025-03-07 22:41:34 -0500

    Added dark mode to navigation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -28,7 +28,7 @@ function classNames(...classes) {
 export default function Navigation() {
   return (
     <div className='fixed w-full z-50'>
-      <Disclosure as='nav' className='bg-gray-100 dark:bg-gray-800 border-gray-200 border-b'>
+      <Disclosure as='nav' className='bg-gray-100 dark:bg-gray-800 border-gray-200 dark:border-gray-700 border-b'>
         <div className='mx-auto px-4 sm:px-6 lg:px-8'>
           <div className='flex h-16 items-center justify-between'>
             <div className='flex items-center'>
@@ -48,7 +48,7 @@ export default function Navigation() {
                       aria-current={item.current ? "page" : undefined}
                       className={classNames(
                         item.current
-                          ? "bg-white text-white"
+                          ? "bg-white dark:bg-black text-white"
                           : "text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-white",
                         "rounded-md px-3 py-2 text-sm font-medium"
                       )}
@@ -71,21 +71,7 @@ export default function Navigation() {
                 </button>
               </div>
             </div>
-            <div className='-mr-2 flex md:hidden'>
-              {/* Mobile menu button */}
-              <DisclosureButton className='group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden'>
-                <span className='absolute -inset-0.5' />
-                <span className='sr-only'>Open main menu</span>
-                <Bars3Icon
-                  aria-hidden='true'
-                  className='block size-6 group-data-open:hidden'
-                />
-                <XMarkIcon
-                  aria-hidden='true'
-                  className='hidden size-6 group-data-open:block'
-                />
-              </DisclosureButton>
-            </div>
+            
           </div>
         </div>
       </Disclosure>
