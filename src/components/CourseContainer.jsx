acb121d864cd67b1095cbd31f61c0c100fdbf3cc
Andy Lin <andy.lin64@qmail.cuny.edu>
2025-03-08 14:26:14 -0500

    "Fixed Course"

--- src/components/CourseContainer.jsx
+++ src/components/CourseContainer.jsx
@@ -1,22 +1,35 @@
 import { Link } from 'react-router-dom';
 import { Button } from '@headlessui/react';
 
-const CourseContainer = ({imageURL, title, description, internalURL}) => {
+const CourseContainer = ({ imageURL, title, description, internalURL, locked }) => {
   return (
     <div className='w-full flex flex-row justify-between items-center p-4 border border-dotted border-gray-200 rounded-lg shadow-md my-4 hover:border-solid hover:border-blue-300'>
       <div className='flex flex-row items-center gap-4'>
-        <img src={imageURL} alt={title+' icon'} className='max-w-[200px]'/>
+        {locked ? (
+          <img
+            src="public/BlueLock.png"
+            alt="Locked"
+            className='max-w-[100px] mr-2 bg-gray-900' // Temporary background to blend white
+          />
+        ) : (
+          <img
+            src={imageURL}
+            alt={title}
+            className='max-w-[100px]'
+          />
+        )}
         <div className='flex flex-col gap-4'>
           <h1 className='text-3xl bold'>{title}</h1>
           <p>{description}</p>
         </div>
       </div>
-      {locked ? (
-        <img src="public/BlueLock.png" alt="Locked course" className='max-w-[50px]' />
-      ) : (
-        <Link to={internalURL} className='py-2 px-4 rounded-md border'>Start Course</Link>
-      )}    
-      </div>
+      <Link
+        to={internalURL}
+        className={`py-2 px-4 rounded-md border ${locked ? 'text-gray-400 border-gray-400 cursor-not-allowed' : 'text-blue-400 border-blue-400'}`}
+      >
+        Start Course
+      </Link>
+    </div>
   )
 
 }
